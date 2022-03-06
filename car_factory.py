06cdf7ec432e66912afa88147ae0f494ac380e5b
Andy Celdo <andyceldo1@gmail.com>
2022-03-06 09:21:14 -0500

    Correcting Car Factory Class
    Did not update the Car Factory Class to match the current parameters of the methods in the engine and battery classes, should be ok now

--- car_factory.py
+++ car_factory.py
@@ -11,40 +11,21 @@ class CarFactory(ABC):
     def __init__(self):
 
     def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        calliope = Car(CapuletEngine, Spindler)
-        calliope.current_date = current_date
-        calliope.last_current_date = last_current_date
-        calliope.current_mileage = current_mileage
-        calliope.last_service_mileage = last_service_mileage
+        calliope = Car(CapuletEngine(current_mileage, last_service_mileage), SpindlerBattery(current_date, last_service_date))
         return calliope
     
     def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        glissade = Car(WilloughbyEngine, SpindlerBattery)
-        glissade.current_date = current_date
-        glissade.last_current_date = last_current_date
-        glissade.current_mileage = current_mileage
-        glissade.last_service_mileage = last_service_mileage
+        glissade = Car(WilloughbyEngine(current_mileage, last_service_mileage), SpindlerBattery(current_date, last_service_date))
         return glissade
 
     def create_palindrome(self, current_date, last_service_date, warning_light_on):
-        palindrome = Car(SternmanEngine, SpindlerBattery)
-        palindrome.current_date = current_date
-        palindrome.last_current_date = last_current_date
-        palindrome.warning_light_on = warning_light_on
+        palindrome = Car(SternmanEngine(warning_light_on), SpindlerBattery(current_date, last_service_date))
         return palindrome
 
     def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        rorschach = Car(WilloughbyEngine, NubbinBattery)
-        rorshcach.current_date = current_date
-        rorshcach.last_current_date = last_current_date
-        rorshcach.current_mileage = current_mileage
-        rorshcach.last_service_mileage = last_service_mileage
+        rorschach = Car(WilloughbyEngine(current_date, last_service_date), NubbinBattery(current_date, last_service_date))
         return rorschach
 
     def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        thovex = Car(CapuletEngine, NubbinBattery)
-        thovex.current_date = current_date
-        thovex.last_current_date = last_current_date
-        thovex.current_mileage = current_mileage
-        thovex.last_service_mileage = last_service_mileage
+        thovex = Car(CapuletEngine(current_date, last_service_date), NubbinBattery(current_date, last_service_date))
         return thovex
